f4acd45e53ebe2f4966f47ab66716386545b076c
asadullahislam <asadullah0072002@gmail.com>
2024-03-06 17:30:59 +0600

    done

--- index.js
+++ index.js
@@ -250,6 +250,7 @@ async function run() {
             // const payments = await paymentCollection.find().toArray();
             // const revenue = payments.reduce((total, payment) => total + payment.price, 0);
 
+
             const result = await paymentCollection.aggregate([
                 {
                     $group: {
@@ -260,8 +261,20 @@ async function run() {
                     }
                 }
             ]).toArray();
-
             const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+            // const result = await paymentCollection.aggregate([
+            //     {
+            //         $group: {
+            //             _id: null,
+            //             totalRevenue: {
+            //                 $sum: '$price'
+            //             }
+            //         }
+            //     }
+            // ]).toArray();
+
+            // const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 
             res.send({
                 users,
